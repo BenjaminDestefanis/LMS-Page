ee2a48304375b0abb37375b1e11077422726925d
BenjaminDestefanis <benjaminisraelb@gmail.com>
2025-02-07 18:06:18 -0300

    New Js

---
+++ landing-page/assets/js/script.js
@@ -0,0 +1,10 @@
+/* const header = document.querySelector("header");
+
+
+
+window.addEventListener("scroll", () => {
+    console.log("FIRST DEBUGG")
+    header.classList.toggle("sticky", windows.scrollY > 0);
+})
+
+console.log("DEBUGG") */

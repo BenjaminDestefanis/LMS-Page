95d6879984e90e488a13289cb9d91eb3f4cb6a83
BenjaminDestefanis <benjaminisraelb@gmail.com>
2025-04-01 12:03:26 -0300

    logo-pestaña

---
+++ ADA-APP/frontend/adadevschool/src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function Header() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1>Header</h1>
+
+    </>
+  )
+}
+
+export default Header;
